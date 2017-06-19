21418aac40c153c2babe95bdd88a3bc9d8844d37
Remo Fritzsche <remo.fritzsche@sitrox.com>
2017-06-19 18:56:55 +0200

    Fix routing integration

--- lib/rails_ops/mixins/routes.rb
+++ lib/rails_ops/mixins/routes.rb
@@ -1,24 +1,25 @@
 module RailsOps::Mixins::Routes
   extend ActiveSupport::Concern
 
-  class RoutingContainer
-    def initialize(url_options)
-      @url_options = url_options
-    end
+  # This class can't be defined at load time of this file as `Rails.application`
+  # would not exist at this point in time. Instead, we're creating and caching
+  # this class on the first call. This is not thread-safe, but the worst case is
+  # that this is performed more than once.
+  def self.container_class
+    @container ||= Class.new do
+      include Rails.application.routes.url_helpers
 
-    def method_missing(name, *args)
-      if Rails.application.routes.url_helpers.respond_to?(name)
-        Rails.application.routes.url_helpers.send(name, *args, @url_options)
-      else
-        super
+      attr_reader :url_options
+
+      def initialize(url_options)
+        @url_options = url_options
       end
-    end
-
-    def respond_to_missing?(*_args)
-      true
     end
   end
 
+  # Returns an object that responds to all URL helper methods using the
+  # `url_options` provided with the operation's context. If no URL options are
+  # given, this method will raise an exception.
   def routes
     unless @routes
       if context.url_options.nil?
@@ -26,7 +27,7 @@ module RailsOps::Mixins::Routes
              'Can not access routes helpers, no url_options given in context.'
       end
 
-      @routes = RoutingContainer.new(context.url_options)
+      @routes = RailsOps::Mixins::Routes.container_class.new(context.url_options)
     end
 
     return @routes
