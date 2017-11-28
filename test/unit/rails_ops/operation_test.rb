c4393288a95eccfbea06eadfd8e0c6f5a83aa236
Remo Fritzsche <remo.fritzsche@sitrox.com>
2017-11-28 14:12:06 +0100

    Extend basic op tests

--- test/unit/rails_ops/operation_test.rb
+++ test/unit/rails_ops/operation_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RailsOps::OperationTest < Minitest::Test
+class RailsOps::OperationTest < ActiveSupport::TestCase
   include TestHelper
 
   EXCEPTION = Class.new(StandardError)
@@ -48,5 +48,86 @@ class RailsOps::OperationTest < Minitest::Test
     end
 
     refute BASIC_OP.run(exception: RailsOps::Exceptions::ValidationFailed.new('A message'))
+  end
+
+  def test_constructor
+    context = RailsOps::Context.new
+
+    # new()
+    op = BASIC_OP.new
+    assert_not_equal context.object_id, op.context.object_id
+    assert_equal({}, op.params)
+
+    # new(nil)
+    op = BASIC_OP.new(nil)
+    assert_not_equal context.object_id, op.context.object_id
+    assert_equal({}, op.params)
+
+    # new(nil, nil)
+    op = BASIC_OP.new(nil)
+    assert_not_equal context.object_id, op.context.object_id
+    assert_equal({}, op.params)
+
+    # new(context, params)
+    op = BASIC_OP.new(context, key: :val)
+    assert_equal context.object_id, op.context.object_id
+    assert_equal({ key: :val }.with_indifferent_access, op.params)
+
+    # new(context)
+    op = BASIC_OP.new(context)
+    assert_equal context.object_id, op.context.object_id
+    assert_equal({}, op.params)
+
+    # new(params)
+    op = BASIC_OP.new(key: :val)
+    assert_not_equal context.object_id, op.context.object_id
+    assert_equal({ key: :val }.with_indifferent_access, op.params)
+
+    # new(params) with ActionController::Parameters
+    params = ActionController::Parameters.new(key: :val)
+    op = BASIC_OP.new(params)
+    assert_not_equal params.object_id, op.params.object_id
+    assert_equal({ key: :val }.with_indifferent_access, op.params)
+  end
+
+  def test_params
+    params = { a: 1, 'b': 1 }
+    op = BASIC_OP.new(params)
+
+    # ---------------------------------------------------------------
+    # Check if op.params and op.osparams are correctly populated
+    # ---------------------------------------------------------------
+    assert_equal params.with_indifferent_access, op.params
+    assert_equal op.params, op.osparams.to_h.with_indifferent_access
+
+    # ---------------------------------------------------------------
+    # Verify that operations work with a duplicate params hash,
+    # and op.params and op.osparams are not connected
+    # ---------------------------------------------------------------
+
+    # Change of outside params hash
+    params[:a] = 2
+    assert_equal 1, op.params[:a]
+    assert_equal 1, op.osparams[:a]
+
+    # Change of op.params hash
+    op.params[:a] = 3
+    assert_equal 2, params[:a]
+    assert_equal 1, op.osparams[:a]
+
+    # Change of op.osparams hash
+    op.osparams.a = 4
+    assert_equal 2, params[:a]
+    assert_equal 3, op.params[:a]
+
+    # ---------------------------------------------------------------
+    # Verify that the params hash is deep duplicated
+    # ---------------------------------------------------------------
+    params = { a: { foo: :bar } }
+    op = Class.new(RailsOps::Operation).new(params)
+    params[:a][:foo] = :baz
+
+    assert_equal :bar, op.params[:a][:foo]
+    assert_equal :bar, op.osparams.a[:foo]
   end
 end
