14270e74b89b9fafc31c3d2b9c1db62ac57d2d5a
Adrian Hirt <aedu.hirt@gmail.com>
2022-02-14 16:59:01 +0100

    Add module option to generator

--- lib/generators/operation/operation_generator.rb
+++ lib/generators/operation/operation_generator.rb
@@ -5,13 +5,23 @@ class OperationGenerator < Rails::Generators::NamedBase
   class_option :skip_views, type: :boolean, desc: "Don't add the views."
   class_option :skip_routes, type: :boolean, desc: "Don't add routes to config/routes.rb."
   class_option :only_operations, type: :boolean, desc: 'Only add the operations. This is equal to specifying --skip-controller --skip-routes --skip-views'
+  class_option :module, type: :string, desc: 'Add the operations in a module, e.g. "Admin" results in namespacing everything in the Admin module'
 
   def add_operations
     @class_name = name.classify
     @underscored_name = name.underscore
     @underscored_pluralized_name = name.underscore.pluralize
 
-    operations_path = "app/operations/#{@underscored_name}"
+    operations_path = 'app/operations/'
+
+    if options[:module].present?
+      @module_name = options[:module].classify
+      @module_underscored_name = @module_name.underscore
+
+      operations_path += "#{@module_underscored_name}/"
+    end
+
+    operations_path += @underscored_name.to_s
 
     template 'load.erb', "#{operations_path}/load.rb"
     template 'create.erb', "#{operations_path}/create.rb"
@@ -22,7 +32,11 @@ class OperationGenerator < Rails::Generators::NamedBase
   def add_controller
     return if options[:skip_controller] || options[:only_operations]
 
-    controller_file_path = "app/controllers/#{@underscored_pluralized_name}_controller.rb"
+    controller_file_path = 'app/controllers/'
+    if @module_underscored_name.present?
+      controller_file_path += "#{@module_underscored_name}/"
+    end
+    controller_file_path += "#{@underscored_pluralized_name}_controller.rb"
     @controller_name = "#{@class_name.pluralize}Controller"
 
     template 'controller.erb', controller_file_path
@@ -31,7 +45,11 @@ class OperationGenerator < Rails::Generators::NamedBase
   def add_views
     return if options[:skip_views] || options[:only_operations]
 
-    views_folder = "app/views/#{@underscored_pluralized_name}"
+    views_folder = 'app/views/'
+    if @module_underscored_name.present?
+      views_folder += "#{@module_underscored_name}/"
+    end
+    views_folder += @underscored_pluralized_name.to_s
 
     %w(index show new edit).each do |view|
       template 'view.erb', "#{views_folder}/#{view}.html.haml"
