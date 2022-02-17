c6029da8f44b3be6d664655944c7968364e5b1e8
Adrian Hirt <aedu.hirt@gmail.com>
2022-02-17 14:32:42 +0100

    Fix sti type not correct on creating of record

--- test/unit/rails_ops/operation/model/sti_test.rb
+++ test/unit/rails_ops/operation/model/sti_test.rb
@@ -34,6 +34,30 @@ class RailsOps::Operation::Model::StiTest < ActiveSupport::TestCase
     end
   end
 
+  UPDATE_DOG_OP = Class.new(RailsOps::Operation::Model::Update) do
+    model Dog do
+      attribute :my_virtual_dog_name
+    end
+  end
+
+  CREATE_DOG_OP = Class.new(RailsOps::Operation::Model::Create) do
+    model Dog do
+      attribute :my_virtual_dog_name
+    end
+  end
+
+  UPDATE_PHOENIX_OP = Class.new(RailsOps::Operation::Model::Update) do
+    model Phoenix do
+      attribute :my_virtual_phoenix_name
+    end
+  end
+
+  CREATE_PHOENIX_OP = Class.new(RailsOps::Operation::Model::Create) do
+    model Phoenix do
+      attribute :my_virtual_phoenix_name
+    end
+  end
+
   def test_load_animal
     model = LOAD_ANIMAL_OP.new(id: @dog.id).model
     assert_equal 'Dog', model.type
@@ -66,5 +90,37 @@ class RailsOps::Operation::Model::StiTest < ActiveSupport::TestCase
     assert_equal 'Phoenix', model.type
     assert model.is_a?(LOAD_PHOENIX_OP.model)
     assert_nothing_raised { model.my_virtual_phoenix_name = 'Lenny' }
+  end
+
+  def test_create_dog
+    op = CREATE_DOG_OP.new.run!
+
+    assert_equal 'Dog', op.model.type
+    assert op.model.is_a?(CREATE_DOG_OP.model)
+    assert_nothing_raised { op.model.my_virtual_dog_name = 'Lenny' }
+  end
+
+  def test_update_dog
+    op = UPDATE_DOG_OP.new(id: @dog.id).run!
+
+    assert_equal 'Dog', op.model.type
+    assert op.model.is_a?(UPDATE_DOG_OP.model)
+    assert_nothing_raised { op.model.my_virtual_dog_name = 'Lenny' }
+  end
+
+  def test_create_phoenix
+    op = CREATE_PHOENIX_OP.new.run!
+
+    assert_equal 'Phoenix', op.model.type
+    assert op.model.is_a?(CREATE_PHOENIX_OP.model)
+    assert_nothing_raised { op.model.my_virtual_phoenix_name = 'Lenny' }
+  end
+
+  def test_update_phoenix
+    op = UPDATE_PHOENIX_OP.new(id: @phoenix.id).run!
+
+    assert_equal 'Phoenix', op.model.type
+    assert op.model.is_a?(UPDATE_PHOENIX_OP.model)
+    assert_nothing_raised { op.model.my_virtual_phoenix_name = 'Lenny' }
   end
 end
